13c215d0319331fb9226ca1f47ca38779c6da211
Vitali Popov <123839092+Etelgast@users.noreply.github.com>
2024-05-24 15:36:56 +0300

    feat: add custom directive v-click-outside

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ClickOutsideDirective from './directives/ClickOutsideDirective'
 
 import App from './App.vue'
 import router from './router'
@@ -7,4 +8,7 @@ import router from './router'
 const app = createApp(App)
 const pinia = createPinia()
 
-export const application = app.use(pinia).use(router)
+export const application = app
+  .use(pinia)
+  .use(router)
+  .directive('click-outside', ClickOutsideDirective)
